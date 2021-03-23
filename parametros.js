dd55caab59a65fd507ac2e1c3e3d3a10e9a87670
Luis-Aguino <luasjaramillohotmail.com>
2021-03-23 11:46:25 -0500

    modificacion

--- parametros.js
+++ parametros.js
@@ -1,11 +1,11 @@
-var getRegister = function(nombre,apellido,email,password) {
-    var nombre = document.getElementByid("nombre").value;
-    var apellido = document.getElementByid("apellido").value;
-    var email = document.getElementByid("email").value;
-    var password = document.getElementByid("password").value;
-    var password = document.getElementByid("password").value;
+let getRegister = function() {
+    let nombre = document.getElementByid("nombre").value;
+    let apellido = document.getElementByid("apellido").value;
+    let email = document.getElementByid("email").value;
+    let password = document.getElementByid("password").value;
+    let password = document.getElementByid("password").value;
    
-    console.log(nombre+" "+nombre)
+    console.log(nombre+" "+password+" "+apellido+" "+email)
 }
 
 
