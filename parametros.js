868342d7e1de42d71eaac78b44a7f834caccaee1
Luis-Aguino <luasjaramillohotmail.com>
2021-03-23 11:12:32 -0500

    modificacion

---
+++ parametros.js
@@ -0,0 +1,11 @@
+var getRegister = function(nombre,apellido,email,password) {
+    var nombre = document.getElementByid("nombre").value;
+    var apellido = document.getElementByid("apellido").value;
+    var email = document.getElementByid("email").value;
+    var password = document.getElementByid("password").value;
+    var password = document.getElementByid("password").value;
+   
+    console.log(nombre+" "+nombre)
+}
+
+
